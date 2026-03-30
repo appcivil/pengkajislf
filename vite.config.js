66510e3cca03391cb2adb8e5fb5100ac726b00c2
Admin SKPSLF <admin.skpslf@gmail.com>
2026-03-30 19:05:00 +0700

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/smartaipengkaji/',
+  base: '/pengkajislf/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
